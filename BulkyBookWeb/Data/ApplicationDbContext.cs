8f126cdb57f3d89de206bc458fda34223da9c7a9
Jeremy Marion Sasis <jeremy.marion.sasis@gmail.com>
2023-01-11 22:45:32 +0800

    Added Entity Framework Plugins, Created connection strings, Added to Dependency container, Created Category Model and Migration

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //define tables
+        public DbSet<Category> Categories { get; set; }
+    }
+}
